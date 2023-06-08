98507c54901ae866dd6871427097aca1c1904e2c
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-06-09 02:29:12 +0300

    Refactoring, decomposition

---
+++ lib/theme/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData(
+  scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+  dividerColor: Colors.white10,
+  appBarTheme: const AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 31, 31, 31),
+      titleTextStyle: TextStyle(
+          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700)),
+  listTileTheme: const ListTileThemeData(iconColor: Colors.white),
+  textTheme: TextTheme(
+    bodyMedium: const TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w500,
+      fontSize: 20,
+    ),
+    labelSmall: TextStyle(
+      color: Colors.white.withOpacity(0.6),
+      fontWeight: FontWeight.w700,
+      fontSize: 14,
+    ),
+  ),
+  useMaterial3: true,
+);
